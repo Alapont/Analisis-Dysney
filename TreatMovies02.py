20cdcde4844ef0293dd22c70592230fe5cb922a5
victordpc <victordpc@gmail.com>
2018-11-12 01:54:52 +0100

    All movies treated whit method 2

--- TreatMovies02.py
+++ TreatMovies02.py
@@ -9,7 +9,8 @@ treatedFolder = 'TreatedScripts'
 movies = ['17Again.txt', 'Coco.txt', 'Frankenweenie.txt', 'Frozen.txt', 'GuardiansOfTheGalaxyVol2.txt',
           'PiratesOfTheCaribbean.txt', 'PiratesOfTheCaribbeanDeadMan\'sChest.txt', 'TheAvengers.txt',
           'TheChroniclesOfNarniaTheLionTheWitchAndTheWardrobe.txt', 'Thor.txt', 'ThorRagnarock.txt',
-          'ToyStory.txt', 'Tron.txt', 'TronLegacy.txt', 'Up.txt', 'Wall-E.txt', 'Zootropia.txt']
+          'ToyStory.txt', 'Tron.txt', 'TronLegacy.txt', 'Up.txt', 'Wall-E.txt', 'Zootropia.txt',
+          'StarWarsEpisodeIIAttackOfTheClones.txt', 'StarWarsEpisodeIVANewHope.txt','StarWarsEpisodeVTheEmpireStrikesBack.txt']
 
 frases = [re.compile(r'^[ ]{20}[\w\.\`\"\-\']', re.IGNORECASE),
           re.compile(r'^[ ]{18}[\w\.\`\"\-\']', re.IGNORECASE),
@@ -27,7 +28,10 @@ frases = [re.compile(r'^[ ]{20}[\w\.\`\"\-\']', re.IGNORECASE),
           re.compile(r'^[ ]{15,20}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
           re.compile(r'^[ ]{20,25}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
           re.compile(r'^[ ]{20,27}[\w\.\`\"\-\'\(\*\?\[]', re.IGNORECASE),
-          re.compile(r'^[ ]{8,12}[\w\.\`\"\-\'\(\*\?\[]', re.IGNORECASE)]
+          re.compile(r'^[ ]{8,12}[\w\.\`\"\-\'\(\*\?\[]', re.IGNORECASE),
+          re.compile(r'^[\t]{3}[\w\.\`\"\-\'\(\*\?\[]', re.IGNORECASE),
+          re.compile(r'^[ ]{20,26}[\w\.\`\"\-\'\(\*\?\[]', re.IGNORECASE),
+          re.compile(r'^[\t]{2,3}[\w\.\`\"\-\'\(\*\?\[\ ]', re.IGNORECASE)]
 
 comentarios = [re.compile(r'^[ ]{10}[\w\.\`\"\-]', re.IGNORECASE),
                re.compile(r'^[ ]{6}[\w\.\`\"\-]', re.IGNORECASE),
@@ -45,7 +49,10 @@ comentarios = [re.compile(r'^[ ]{10}[\w\.\`\"\-]', re.IGNORECASE),
                re.compile(r'^[ ]{0,6}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
                re.compile(r'^[ ]{0,10}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
                re.compile(r'^[ ]{0,12}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
-               re.compile(r'^[ ]{0,4}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE)]
+               re.compile(r'^[ ]{0,4}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{0,4}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{10,16}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{0}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE)]
 
 numPag = [re.compile(r'\d+.', re.IGNORECASE),
           re.compile(r'\d+.', re.IGNORECASE),
@@ -63,11 +70,14 @@ numPag = [re.compile(r'\d+.', re.IGNORECASE),
           re.compile(r'\d+', re.IGNORECASE),
           re.compile(r'\d+.', re.IGNORECASE),
           re.compile(r'\d+.', re.IGNORECASE),
+          re.compile(r'\d+.', re.IGNORECASE),
+          re.compile(r'\d+.', re.IGNORECASE),
+          re.compile(r'\d+.', re.IGNORECASE),
           re.compile(r'\d+.', re.IGNORECASE)]
 
 parentesis = re.compile(r'\([\w\d\s\.\,\-\'\"\/\:\;\?\!\#]*\)', re.IGNORECASE)
 
-cabecera = [33, 31, 25, 13, 34, 8, 20, 80, 26, 36, 34, 55, 16, 80, 17, 128, 32]
+cabecera = [33, 31, 25, 13, 34, 8, 20, 80, 26, 36, 34, 55, 16, 80, 17, 128, 32, 42, 60, 14]
 
 for x in range(len(movies)):
     indice = 0
