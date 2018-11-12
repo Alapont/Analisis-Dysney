306bc842b038c05f188c60ccd4525f611debb3f9
victordpc <victordpc@gmail.com>
2018-11-12 02:51:23 +0100

    Fix some extra new lines

--- TreatMovies02.py
+++ TreatMovies02.py
@@ -10,35 +10,35 @@ movies = ['17Again.txt', 'Coco.txt', 'Frankenweenie.txt', 'Frozen.txt', 'Guardia
           'PiratesOfTheCaribbean.txt', 'PiratesOfTheCaribbeanDeadMan\'sChest.txt', 'TheAvengers.txt',
           'TheChroniclesOfNarniaTheLionTheWitchAndTheWardrobe.txt', 'Thor.txt', 'ThorRagnarock.txt',
           'ToyStory.txt', 'Tron.txt', 'TronLegacy.txt', 'Up.txt', 'Wall-E.txt', 'Zootropia.txt',
-          'StarWarsEpisodeIIAttackOfTheClones.txt', 'StarWarsEpisodeIVANewHope.txt','StarWarsEpisodeVTheEmpireStrikesBack.txt']
+          'StarWarsEpisodeIIAttackOfTheClones.txt', 'StarWarsEpisodeIVANewHope.txt', 'StarWarsEpisodeVTheEmpireStrikesBack.txt']
 
-frases = [re.compile(r'^[ ]{20}[\w\.\`\"\-\']', re.IGNORECASE),
+frases = [re.compile(r'^[ ]{20,25}[\w\.\`\"\-\'\()]', re.IGNORECASE),
           re.compile(r'^[ ]{18}[\w\.\`\"\-\']', re.IGNORECASE),
-          re.compile(r'^[ ]{2}[\w\.\`\"\-\']', re.IGNORECASE),
-          re.compile(r'^[ ]{18}[\w\.\`\"\-\']', re.IGNORECASE),
+          re.compile(r'^[ ]{2,3}[\w\.\`\"\-\'\ ]|^[\t]{1,3}[\w\.\`\"\-\'\(\ ]', re.IGNORECASE),
+          re.compile(r'^[ ]{18,23}[\w\.\`\"\-\'\(]', re.IGNORECASE),
           re.compile(r'^[ ]{16,35}[\w\.\`\"\-\']', re.IGNORECASE),
           re.compile(r'^[ ]{10,18}[\w\.\`\"\-\'\(]', re.IGNORECASE),
-          re.compile(r'^[ ]{20,26}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
+          re.compile(r'^[ ]{20,26}[\w\.\`\"\-\'\(\*\?\#]', re.IGNORECASE),
           re.compile(r'^[ ]{11}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
           re.compile(r'^[ ]{20,26}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
-          re.compile(r'^[ ]{10}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
+          re.compile(r'^[ ]{10,11}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
           re.compile(r'^[ ]{10,18}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
-          re.compile(r'^[ ]{12}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
-          re.compile(r'^[ ]{20}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
+          re.compile(r'^[ ]{12,20}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
+          re.compile(r'^[ ]{20,30}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
           re.compile(r'^[ ]{15,20}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
           re.compile(r'^[ ]{20,25}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
-          re.compile(r'^[ ]{20,27}[\w\.\`\"\-\'\(\*\?\[]', re.IGNORECASE),
+          re.compile(r'^[ ]{20,35}[\w\.\`\"\-\'\(\*\?\[]', re.IGNORECASE),
           re.compile(r'^[ ]{8,12}[\w\.\`\"\-\'\(\*\?\[]', re.IGNORECASE),
           re.compile(r'^[\t]{3}[\w\.\`\"\-\'\(\*\?\[]', re.IGNORECASE),
           re.compile(r'^[ ]{20,26}[\w\.\`\"\-\'\(\*\?\[]', re.IGNORECASE),
           re.compile(r'^[\t]{2,3}[\w\.\`\"\-\'\(\*\?\[\ ]', re.IGNORECASE)]
 
-comentarios = [re.compile(r'^[ ]{10}[\w\.\`\"\-]', re.IGNORECASE),
-               re.compile(r'^[ ]{6}[\w\.\`\"\-]', re.IGNORECASE),
-               re.compile(r'^[ ]{0}[\w\.\`\"\-]', re.IGNORECASE),
-               re.compile(r'^[ ]{3}[\w\.\`\"\-]', re.IGNORECASE),
-               re.compile(r'^[ ]{10}[\w\.\`\"\-]', re.IGNORECASE),
-               re.compile(r'^[ ]{0}[\w\.\`\"\-\,]', re.IGNORECASE),
+comentarios = [re.compile(r'^[ ]{10}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{6}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{0}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{3}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{10}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{0}[\w\.\`\"\-\,\[\?\#\{]', re.IGNORECASE),
                re.compile(r'^[ ]{14,15}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
                re.compile(r'^[ ]{10}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
                re.compile(r'^[ ]{14,15}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
@@ -77,7 +77,8 @@ numPag = [re.compile(r'\d+.', re.IGNORECASE),
 
 parentesis = re.compile(r'\([\w\d\s\.\,\-\'\"\/\:\;\?\!\#]*\)', re.IGNORECASE)
 
-cabecera = [33, 31, 25, 13, 34, 8, 20, 80, 26, 36, 34, 55, 16, 80, 17, 128, 32, 42, 60, 14]
+cabecera = [33, 31, 25, 13, 34, 8, 20, 80, 26,
+            36, 34, 55, 16, 80, 17, 128, 32, 42, 60, 14]
 
 for x in range(len(movies)):
     indice = 0
