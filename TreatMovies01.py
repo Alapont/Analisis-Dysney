9bd74f6ab58fef377ffed9cc9542ad08a7499bb3
victordpc <victordpc@gmail.com>
2018-11-12 01:09:08 +0100

    More movies treated

--- TreatMovies01.py
+++ TreatMovies01.py
@@ -8,14 +8,13 @@ charPattern = r'[\w]{1}[\w \']+:'
 
 moviesFolder = 'Scripts'
 treatedFolder = 'TreatedScripts'
-movies = ['BeautyAndTheBeast.txt', 'Aladdin.txt', 'HunchbackOfNotreDame.txt', 'LittleMermaid.txt',
-          'MaryPoppins.txt', 'AGoofyMovie.txt', 'TheLionKing.txt', 'TheRescuersDownUnder.txt', 'TheSleepingBeauty.txt']
-cabecera = [6, 64, 26, 3, 1, 10, 21, 34, 0]
+movies = ['BeautyAndTheBeast.txt', 'Aladdin.txt', 'HunchbackOfNotreDame.txt', 'LittleMermaid.txt', 'MaryPoppins.txt',
+          'AGoofyMovie.txt', 'TheLionKing.txt', 'TheRescuersDownUnder.txt', 'TheSleepingBeauty.txt', 'Mulan.txt']
+cabecera = [6, 64, 26, 3, 1, 10, 21, 34, 0, 8]
 
 for x in range(len(movies)):
     indice = 0
     tratado = []
-    ignorar = False
     with open(os.path.join(os.path.join(os.getcwd(), moviesFolder), movies[x]), 'r') as reader:
         # Eliminamos las lineas introductorias
         for z in range(cabecera[x]):
@@ -32,8 +31,7 @@ for x in range(len(movies)):
                     indice += 1
                 else:
                     # Encontramos una linea y decidimos donde meterla
-                    if(ignorar == False):
-                        tratado[indice-1] = tratado[indice-1]+' '+line.strip()
+                    tratado[indice-1] = tratado[indice-1]+' '+line.strip()
 
     # Guardamos los resultados en un fichero
     with open(os.path.join(os.path.join(os.getcwd(), treatedFolder), movies[x]), 'w') as writer:
---
+++ TreatMovies02.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+
+moviesFolder = 'Scripts'
+treatedFolder = 'TreatedScripts'
+movies = ['17Again.txt', 'Coco.txt', 'Frankenweenie.txt', 'Frozen.txt', 'GuardiansOfTheGalaxyVol2.txt',
+          'PiratesOfTheCaribbean.txt', 'PiratesOfTheCaribbeanDeadMan\'sChest.txt', 'TheAvengers.txt',
+          'TheChroniclesOfNarniaTheLionTheWitchAndTheWardrobe.txt', 'Thor.txt', 'ThorRagnarock.txt',
+          'ToyStory.txt', 'Tron.txt', 'TronLegacy.txt', 'Up.txt', 'Wall-E.txt', 'Zootropia.txt']
+
+frases = [re.compile(r'^[ ]{20}[\w\.\`\"\-\']', re.IGNORECASE),
+          re.compile(r'^[ ]{18}[\w\.\`\"\-\']', re.IGNORECASE),
+          re.compile(r'^[ ]{2}[\w\.\`\"\-\']', re.IGNORECASE),
+          re.compile(r'^[ ]{18}[\w\.\`\"\-\']', re.IGNORECASE),
+          re.compile(r'^[ ]{16,35}[\w\.\`\"\-\']', re.IGNORECASE),
+          re.compile(r'^[ ]{10,18}[\w\.\`\"\-\'\(]', re.IGNORECASE),
+          re.compile(r'^[ ]{20,26}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
+          re.compile(r'^[ ]{11}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
+          re.compile(r'^[ ]{20,26}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
+          re.compile(r'^[ ]{10}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
+          re.compile(r'^[ ]{10,18}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
+          re.compile(r'^[ ]{12}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
+          re.compile(r'^[ ]{20}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
+          re.compile(r'^[ ]{15,20}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
+          re.compile(r'^[ ]{20,25}[\w\.\`\"\-\'\(\*\?]', re.IGNORECASE),
+          re.compile(r'^[ ]{20,27}[\w\.\`\"\-\'\(\*\?\[]', re.IGNORECASE),
+          re.compile(r'^[ ]{8,12}[\w\.\`\"\-\'\(\*\?\[]', re.IGNORECASE)]
+
+comentarios = [re.compile(r'^[ ]{10}[\w\.\`\"\-]', re.IGNORECASE),
+               re.compile(r'^[ ]{6}[\w\.\`\"\-]', re.IGNORECASE),
+               re.compile(r'^[ ]{0}[\w\.\`\"\-]', re.IGNORECASE),
+               re.compile(r'^[ ]{3}[\w\.\`\"\-]', re.IGNORECASE),
+               re.compile(r'^[ ]{10}[\w\.\`\"\-]', re.IGNORECASE),
+               re.compile(r'^[ ]{0}[\w\.\`\"\-\,]', re.IGNORECASE),
+               re.compile(r'^[ ]{14,15}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{10}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{14,15}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{0}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{0,8}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{0}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{0,9}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{0,6}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{0,10}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{0,12}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE),
+               re.compile(r'^[ ]{0,4}[\w\.\`\"\-\,\[\?\#]', re.IGNORECASE)]
+
+numPag = [re.compile(r'\d+.', re.IGNORECASE),
+          re.compile(r'\d+.', re.IGNORECASE),
+          re.compile(r'\d+.', re.IGNORECASE),
+          re.compile(r'\d+.', re.IGNORECASE),
+          re.compile(r'\d+', re.IGNORECASE),
+          re.compile(r'\d+', re.IGNORECASE),
+          re.compile(r'\d+', re.IGNORECASE),
+          re.compile(r'\d+', re.IGNORECASE),
+          re.compile(r'\d+', re.IGNORECASE),
+          re.compile(r'\d+', re.IGNORECASE),
+          re.compile(r'\d+', re.IGNORECASE),
+          re.compile(r'\d+', re.IGNORECASE),
+          re.compile(r'\d+', re.IGNORECASE),
+          re.compile(r'\d+', re.IGNORECASE),
+          re.compile(r'\d+.', re.IGNORECASE),
+          re.compile(r'\d+.', re.IGNORECASE),
+          re.compile(r'\d+.', re.IGNORECASE)]
+
+parentesis = re.compile(r'\([\w\d\s\.\,\-\'\"\/\:\;\?\!\#]*\)', re.IGNORECASE)
+
+cabecera = [33, 31, 25, 13, 34, 8, 20, 80, 26, 36, 34, 55, 16, 80, 17, 128, 32]
+
+for x in range(len(movies)):
+    indice = 0
+    tratado = []
+
+    with open(os.path.join(os.path.join(os.getcwd(), moviesFolder), movies[x]), 'r') as reader:
+        # Eliminamos las lineas introductorias
+        for z in range(cabecera[x]):
+            reader.readline()
+
+        # Tratamos el fichero
+        for line in reader:
+
+            if line.strip() != '':
+                # Encontramos indicaciones de escena
+                if(comentarios[x].match(line) == None) & (numPag[x].match(line.strip()) == None) & (parentesis.match(line.strip()) == None):
+
+                    matchObj = frases[x].match(line)
+                    if matchObj:
+                        # Encontramos una linea y decidimos donde meterla
+                        tratado[indice-1] = tratado[indice-1] + \
+                            ' '+line.strip()
+                    else:
+                        # Encontramos una frase
+                        tratado.append(line.strip()+':')
+                        indice += 1
+
+    # Guardamos los resultados en un fichero
+    with open(os.path.join(os.path.join(os.getcwd(), treatedFolder), movies[x]), 'w') as writer:
+        for y in range(indice):
+            writer.write(tratado[y]+'\n')
