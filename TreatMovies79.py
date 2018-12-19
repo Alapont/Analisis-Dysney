c4e7496d0a29a36509a6a2be7dd35e2f323636f4
victordpc <victordpc@gmail.com>
2018-12-20 00:04:28 +0100

    In to the woods

---
+++ TreatMovies79.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from operator import itemgetter, attrgetter, methodcaller
+import csv
+import os
+import re
+
+genders = dict(men=0, woman=0)
+womens = ["WOMAN", 'GIRL', "STEPMOTHER",'BAKER WIFE','RAPUNZEL','WITCH'
+          ,'WITCH & ALL','WIFE/CINDERELLA','WIFE CINDERELLA','STEPMOTHER/STEPSISTERS'
+          ,"JACK'S MOTHER",'CINDERELLA','LRRH/CINDERELLA','GRANNY','STEPSISTERS','WIFE','LUCINDA','BAKER/WIFE'
+          ,"C'S MOTHER",'FLORINDA','WIFE/BAKER']
+
+origin = 'CountWords'
+destination = 'GendersCount'
+
+filename = 'IntoTheWoods.csv'
+
+if os.path.exists(os.path.join(os.getcwd(), destination)) == False:
+    os.mkdir(os.path.join(os.getcwd(), destination))
+
+with open(os.path.join(os.path.join(os.getcwd(), origin), filename), 'r') as reader:
+    for z in range(2):
+        reader.readline()
+    for line in reader:
+
+        datos = line.split(';')
+        if womens.count(datos[0].upper()) > 0:
+            genders["woman"] = genders["woman"] + int(datos[1])
+        elif datos[0] == '' or datos[1] == '':
+            pass
+        else:
+            genders["men"] = genders["men"] + int(datos[1])
+
+with open(os.path.join(os.path.join(os.getcwd(), destination), filename), 'w') as writer:
+    writer.write(filename[:-(len(filename)-filename.find('.'))] + '\n')
+
+    for key, value in genders.items():
+        writer.write(key + ';' + str(value)+';\n')
+
+
+del genders
