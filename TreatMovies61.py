4496eb9f6564b8764de98d4b54f49900e2e11cfd
victordpc <victordpc@gmail.com>
2018-12-19 21:46:51 +0100

    CampRock

---
+++ TreatMovies61.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from operator import itemgetter, attrgetter, methodcaller
+import csv
+import os
+import re
+
+genders = dict(men=0, woman=0)
+womens = ["MITCHIE", 'TESS SINGING', 'SCREAMING GIRLS', 'LOLA','CAITLYN YELLS','SIERRA','CAITLYN','ELLA','PEGGY'
+,'CONNIE','SCREAMING GIRLS','MITCHIE/CAITLYN','CAITLYN/MITCHIE','DEE LA DUKE']
+
+origin = 'CountWords'
+destination = 'GeneresCount'
+
+filename = 'CampRock.csv'
+
+if os.path.exists(os.path.join(os.getcwd(), destination)) == False:
+    os.mkdir(os.path.join(os.getcwd(), destination))
+
+with open(os.path.join(os.path.join(os.getcwd(), origin), filename), 'r') as reader:
+    for z in range(2):
+        reader.readline()
+    for line in reader:
+
+        datos = line.split(';')
+        if womens.count(datos[0].upper()) > 0:
+            genders["woman"] = genders["woman"] + int(datos[1])
+        else:
+            genders["men"] = genders["men"] + int(datos[1])
+
+with open(os.path.join(os.path.join(os.getcwd(), destination), filename), 'w') as writer:
+    writer.write(filename[:-(len(filename)-filename.find('.'))] + '\n')
+
+    for key, value in genders.items():
+        writer.write(key + ';' + str(value)+';\n')
+
+
+del genders
