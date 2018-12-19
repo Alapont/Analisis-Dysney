16db8590f5e119c2feebce10c4180ce33eca5ee1
victordpc <victordpc@gmail.com>
2018-12-19 22:10:47 +0100

    Capitan America CampRock 2

---
+++ TreatMovies63.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from operator import itemgetter, attrgetter, methodcaller
+import csv
+import os
+import re
+
+genders = dict(men=0, woman=0)
+womens = ["ZEMO'S WIFE", 'NATASHA ROMANOFF', 'MAY PARKER', 'MRS SPENCER','MARIA STARK','SHARON CARTER','MRS. SPENCER','WANDA MAXIMOFF']
+
+origin = 'CountWords'
+destination = 'GeneresCount'
+
+filename = 'CaptainAmericaCivilWar.csv'
+
+if os.path.exists(os.path.join(os.getcwd(), destination)) == False:
+    os.mkdir(os.path.join(os.getcwd(), destination))
+
+with open(os.path.join(os.path.join(os.getcwd(), origin), filename), 'r') as reader:
+    for z in range(2):
+        reader.readline()
+    for line in reader:
+
+        datos = line.split(';')
+        if womens.count(datos[0].upper()) > 0:
+            genders["woman"] = genders["woman"] + int(datos[1])
+        else:
+            genders["men"] = genders["men"] + int(datos[1])
+
+with open(os.path.join(os.path.join(os.getcwd(), destination), filename), 'w') as writer:
+    writer.write(filename[:-(len(filename)-filename.find('.'))] + '\n')
+
+    for key, value in genders.items():
+        writer.write(key + ';' + str(value)+';\n')
+
+
+del genders
