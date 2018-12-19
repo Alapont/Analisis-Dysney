cbfebeb42564ba2ea1fda2153e35ccef39415f14
victordpc <victordpc@gmail.com>
2018-12-19 20:59:27 +0100

    Conteo de palabras

--- TreatMovies40.py
+++ TreatMovies40.py
@@ -44,35 +44,8 @@ for filename in os.listdir(os.path.join(os.getcwd(), moviesFolder)):
 
             for clave in data.keys():
                 if data[clave] > 0:
-                    writer.write(str(clave) + ';' + str(data[clave]) + '\n')
+                    writer.write(str(clave).upper() + ';' + str(data[clave]) + ';\n')
 
         del data
 
 
-# for x in range(3):
-#     with open(os.path.join(os.path.join(os.getcwd(), moviesFolder), pelis[x]), 'r') as reader:
-#         for line in reader:
-#             # if line == blankLine:
-#             #     newLine = True
-
-#             # newScene = re.match(scenePattern, line)
-#             # if newLine && newScene:
-
-#             matchObj = re.match(charPattern, line)
-#             if matchObj:
-#                 character = matchObj.group()[:-1]
-#                 if data.has_key(character):
-#                     data[character] = data[character]+1
-#                 else:
-#                     data[character] = 1
-
-#                 if womens.count(character.upper()) > 0:
-#                     genders["woman"] = genders["woman"]+1
-#                 else:
-#                     genders["men"] = genders["men"]+1
-
-#     for key, value in sorted(data.items(), key=itemgetter(1), reverse=True):
-#         print(key + " - " + str(value))
-
-#     for key, value in genders.items():
-#         print(key + " - " + str(value))
