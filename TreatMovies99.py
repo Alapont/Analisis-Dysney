cf6592126ec1ecc745723f5568e67332b22b3e33
victordpc <victordpc@gmail.com>
2018-12-20 01:30:40 +0100

    The last Jedi

---
+++ TreatMovies99.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from operator import itemgetter, attrgetter, methodcaller
+import csv
+import os
+import re
+
+genders = dict(men=0, woman=0)
+womens = ["WOMAN", 'GIRL', "REY",'HOLDO','MAZ','CAPTAIN PHASMA'
+,'FEMALE P.A. ANNOUNCER','JAYCRIS TUBBS','PHASMA','PAIGE'
+,'LEIA AND AMILYN HOLDO','LEIA IN HOLOGRAM','ROSE'
+,'LARMA','LOVEY','AMILYN HOLDO','COVA NELL','FEMALE VOICE','FINN AND ROSE','RESISTANCE FEMALE SOLDIER'
+,'MAZ',"LARMA D'ACY",'LEIA']
+
+origin = 'CountWords'
+destination = 'GendersCount'
+
+filename = "TheLastJedi.csv"
+
+if os.path.exists(os.path.join(os.getcwd(), destination)) == False:
+    os.mkdir(os.path.join(os.getcwd(), destination))
+
+with open(os.path.join(os.path.join(os.getcwd(), origin), filename), 'r') as reader:
+    for z in range(2):
+        reader.readline()
+    for line in reader:
+
+        datos = line.split(';')
+        if womens.count(datos[0].upper()) > 0:
+            genders["woman"] = genders["woman"] + int(datos[1])
+        elif datos[0] == '' or datos[1] == '':
+            pass
+        else:
+            genders["men"] = genders["men"] + int(datos[1])
+
+with open(os.path.join(os.path.join(os.getcwd(), destination), filename), 'w') as writer:
+    writer.write(filename[:-(len(filename)-filename.find('.'))] + '\n')
+
+    for key, value in genders.items():
+        writer.write(key + ';' + str(value)+';\n')
+
+
+del genders
